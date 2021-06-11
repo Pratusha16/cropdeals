983ad16b45a9ccc4c403ec732455055067597c01
Pratusha16 <pratushah16@gmail.com>
2021-06-11 15:27:19 +0530

    testing

--- BackEnd/dealer/DealerServer.js
+++ BackEnd/dealer/DealerServer.js
@@ -293,4 +293,6 @@ app.use((error,req,res,next)=>{
     })
 })
 
-app.listen("7000",()=>console.log("dealer server is running on 7000"))
+var dealer=app.listen("7000",()=>console.log("dealer server is running on 7000"))
+
+module.exports=dealer;
--- BackEnd/farmer/FarmerServer.js
+++ BackEnd/farmer/FarmerServer.js
@@ -22,42 +22,7 @@ app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json());
 app.use(morgan("dev"));
 
-// app.js
-const swaggerJSDoc = require('swagger-jsdoc');
-const swaggerUi = require('swagger-ui-express');
 
-const swaggerDefinition = {
-    openapi: '3.0.0',
-    info: {
-      title: 'Express API for JSONPlaceholder',
-      version: '1.0.0',
-      description:
-        'This is a REST API application made with Express. It retrieves data from JSONPlaceholder.',
-      license: {
-        name: 'Licensed Under MIT',
-        url: 'https://spdx.org/licenses/MIT.html',
-      },
-      contact: {
-        name: 'JSONPlaceholder',
-        url: 'https://jsonplaceholder.typicode.com',
-      },
-    },
-    servers: [
-      {
-        url: 'http://localhost:5000',
-        description: 'Development server',
-      },
-    ],
-  };
-
-const options = {
-  swaggerDefinition,
-  // Paths to files containing OpenAPI definitions
-  apis: ['./FarmerServer/*.js'],
-};
-
-const swaggerSpec = swaggerJSDoc(options);
-app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 //for browsers only setting headers
 app.use((req,res,next)=>{
@@ -107,74 +72,21 @@ app.get("/farmers",CheckAuth,code.farmers_get_all)
 // fetch particular farmer details with name
 app.get('/farmers/:id',CheckAuth,code.farmers_get_by_id);
 
-/**
- * @swagger
- * /farmers:
- * post:
- * discription: farmers_login
- * parameters:
- * -name:String
- * email:String
- * password:String
- * responses:
- * 200:
- * description:Success
- */
 
 // login dealer user
 app.post("/login",code.farmers_login);
 
-/**
- * @swagger
- * /farmers:
- * post:
- * discription: farmers_register
- * parameters:
- * -name:String
- * email:String
- * password:String
- * contact:Number
- *  gender:String
- *  cropsgrown:Array
- * bank_details:bank
- * responses:
- * 200:
- * description:Success
- */
+
 
 //registering new farmer details
 app.post("/register",code.farmers_register)  
 
-/**
- * @swagger
- * /farmers:
- * put:
- * discription: farmers_get_by_id
- * parameters:
- * -name:String
- * email:String
- * password:String
- * responses:
- * 200:
- * description:Success
- */
+
 
 //updating a particular crop
 app.put("/farmers/:id",CheckAuth,code.farmers_edit_by_id)
 
-/**
- * @swagger
- * /farmers:
- * delete:
- * discription: farmers_get_by_id
- * parameters:
- * -name:String
- * email:String
- * password:String
- * responses:
- * 200:
- * description:Success
- */
+
 
 //deleteing particular crop
 app.delete('/farmers/:id',CheckAuth,code.farmers_delete_by_id)
